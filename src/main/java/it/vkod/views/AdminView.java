e41d795454c2625acc01752173e6383113e1510f
yilmazchef <yilmaz@mail.be>
2021-12-05 04:06:05 +0100

    admin page is improved.

--- src/main/java/it/vkod/views/AdminView.java
+++ src/main/java/it/vkod/views/AdminView.java
@@ -1,10 +1,12 @@
 package it.vkod.views;
 
 
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import it.vkod.api.ExportController;
 import it.vkod.services.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -23,14 +25,34 @@ public class AdminView extends VerticalLayout {
 
         this.adminService = adminService;
 
-        final AtomicLong deleteCounter = new AtomicLong();
+        final var managementLayout = new VerticalLayout();
 
+
+        final AtomicLong deleteCounter = new AtomicLong();
         final var flushButton = new Button("Flush Database", onClick -> {
             long deletedCount = adminService.flushDatabase();
             deleteCounter.set(deletedCount);
         });
 
-        add(flushButton);
+        managementLayout.add(flushButton);
+
+        final var actionLayout = new VerticalLayout();
+
+        final var pdfButton = new Button("Export PDF", onClick -> {
+            UI.getCurrent().navigate(ExportController.EXPORT_CHECKS_PDF_URI);
+        });
+
+        final var csvButton = new Button("Export CSV", onClick -> {
+            UI.getCurrent().navigate(ExportController.EXPORT_CHECKS_CSV_URI);
+        });
+
+        final var excelButton = new Button("Export EXCEL", onClick -> {
+            UI.getCurrent().navigate(ExportController.EXPORT_CHECKS_EXCEL_URI);
+        });
+
+        actionLayout.add(pdfButton, csvButton, excelButton);
+
+        add(managementLayout, actionLayout);
 
     }
 
