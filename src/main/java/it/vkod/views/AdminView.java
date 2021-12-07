d024ff5c56a6229bd9ca3153e33ecab1a00bb2e1
yilmazchef <yilmaz@mail.be>
2021-12-08 00:49:26 +0100

    admin view is under development.
    async mechanism is being tested in a sandboxed environment

--- src/main/java/it/vkod/views/AdminView.java
+++ src/main/java/it/vkod/views/AdminView.java
@@ -7,6 +7,7 @@ import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import it.vkod.services.AdminService;
+import lombok.Value;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.annotation.security.RolesAllowed;
@@ -17,11 +18,13 @@ import java.util.concurrent.atomic.AtomicLong;
 @RolesAllowed({"ADMIN", "MANAGER", "LEADER"})
 public class AdminView extends VerticalLayout {
 
+    @Value("${hostname}")
+    private String hostname;
+
     private final AdminService adminService;
-    private static final String HOST_URL = System.getenv("CHECKINOUT_HOST_URL");
-    private static final String URL_PDF = HOST_URL + "/checks/pdf";
-    private static final String URL_CSV = HOST_URL + "/checks/csv";
-    private static final String URL_EXCEL = HOST_URL + "/checks/excel";
+    private static final String URL_PDF = "/checks/pdf";
+    private static final String URL_CSV = "/checks/csv";
+    private static final String URL_EXCEL = "/checks/excel";
 
     public AdminView(@Autowired AdminService adminService) {
 
@@ -39,9 +42,9 @@ public class AdminView extends VerticalLayout {
 
         final var actionLayout = new VerticalLayout();
 
-        Anchor pdfAnchor = new Anchor(URL_PDF, "Exporteer als PDF");
-        Anchor csvAnchor = new Anchor(URL_CSV, "Exporteer als CSV");
-        Anchor excelAnchor = new Anchor(URL_EXCEL, "Exporteer als PDF");
+        Anchor pdfAnchor = new Anchor(hostname + URL_PDF, "Exporteer als PDF");
+        Anchor csvAnchor = new Anchor(hostname + URL_CSV, "Exporteer als CSV");
+        Anchor excelAnchor = new Anchor(hostname + URL_EXCEL, "Exporteer als PDF");
 
         actionLayout.add(pdfAnchor, csvAnchor, excelAnchor);
 
