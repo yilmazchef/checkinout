aec07cf25255e20cc057c3cf7ef5ac2ed913d4a5
yilmazchef <yilmaz@mail.be>
2021-12-08 01:08:31 +0100

    admin view is under development.

--- src/main/java/it/vkod/views/AdminView.java
+++ src/main/java/it/vkod/views/AdminView.java
@@ -2,12 +2,15 @@ package it.vkod.views;
 
 
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.dialog.Dialog;
 import com.vaadin.flow.component.html.Anchor;
+import com.vaadin.flow.component.html.H4;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import it.vkod.services.AdminService;
-import lombok.Value;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.annotation.security.RolesAllowed;
@@ -18,10 +21,10 @@ import java.util.concurrent.atomic.AtomicLong;
 @RolesAllowed({"ADMIN", "MANAGER", "LEADER"})
 public class AdminView extends VerticalLayout {
 
-    @Value("${hostname}")
-    private String hostname;
 
+    private final String hostname;
     private final AdminService adminService;
+
     private static final String URL_PDF = "/checks/pdf";
     private static final String URL_CSV = "/checks/csv";
     private static final String URL_EXCEL = "/checks/excel";
@@ -29,13 +32,37 @@ public class AdminView extends VerticalLayout {
     public AdminView(@Autowired AdminService adminService) {
 
         this.adminService = adminService;
+        this.hostname = System.getenv("hostname");
+
+        Notification.show("The app is running on " + hostname, 5000, Notification.Position.TOP_CENTER).open();
 
         final var managementLayout = new VerticalLayout();
 
         final AtomicLong deleteCounter = new AtomicLong();
         final var flushButton = new Button("Flush Database", onClick -> {
-            long deletedCount = adminService.flushDatabase();
-            deleteCounter.set(deletedCount);
+            final var flushLayout = new VerticalLayout();
+            final var confirmationDialog = new Dialog();
+            confirmationDialog.setCloseOnEsc(true);
+            confirmationDialog.setOpened(true);
+            final var info = new H4("Please confirm database flush operation?");
+            flushLayout.add(info);
+            final var confirmationLayout = new HorizontalLayout();
+            final var yesButton = new Button("Yes", yesClicked -> {
+                long deletedCount = adminService.flushDatabase();
+                deleteCounter.set(deletedCount);
+                confirmationDialog.close();
+            });
+            final var noButton = new Button("No", noClicked -> {
+                deleteCounter.lazySet(0);
+                confirmationDialog.close();
+            });
+
+            confirmationLayout.add(yesButton, noButton);
+            flushLayout.add(confirmationLayout);
+            confirmationDialog.add(confirmationLayout);
+
+            add(flushLayout);
+
         });
 
         managementLayout.add(flushButton);
