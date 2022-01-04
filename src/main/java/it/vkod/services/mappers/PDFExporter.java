9f3710912f186bed68c653c44a06324aa1713111
yilmazchef <yilmaz@mail.be>
2022-01-04 04:01:34 +0100

    PDF print is fixed.

--- src/main/java/it/vkod/services/mappers/PDFExporter.java
+++ src/main/java/it/vkod/services/mappers/PDFExporter.java
@@ -18,123 +18,126 @@ import java.io.IOException;
 import java.time.LocalDate;
 import java.util.List;
 
+import static com.lowagie.text.PageSize.A4;
+
 @RequiredArgsConstructor
 @Component
 public class PDFExporter {
 
-	public void export( HttpServletResponse response, List< Check > checks ) throws IOException {
+    public void export(HttpServletResponse response, List<Check> checks) throws IOException {
 
-		final var document = new Document( PageSize.A4 );
-		PdfWriter.getInstance( document, response.getOutputStream() );
+        final var document = new Document(A4);
+        PdfWriter.getInstance(document, response.getOutputStream());
 
-		document.open();
-		final var hFont = FontFactory.getFont( FontFactory.HELVETICA_BOLD );
-		hFont.setSize( 14 );
-		hFont.setColor( Color.BLUE );
+        document.open();
+        final var hFont = FontFactory.getFont(FontFactory.HELVETICA_BOLD);
+        hFont.setSize(14);
+        hFont.setColor(Color.BLUE);
 
-		final var hi = "Aanmelden van " + LocalDate.now();
-		final var h = new Paragraph( hi, hFont );
-		h.setSpacingBefore( 120F );
-		h.setSpacingAfter( 40F );
+        final var hi = "Aanmelden van " + LocalDate.now();
+        final var h = new Paragraph(hi, hFont);
+        h.setSpacingBefore(120F);
+        h.setSpacingAfter(40F);
 
-		document.add( h );
+        document.add(h);
 
-		var imageFile = new ClassPathResource( "META-INF/resources/images/logo-for-pdf.png" );
-		var imgB = StreamUtils.copyToByteArray( imageFile.getInputStream() );
-		final var img = Image.getInstance( imgB );
-		img.setSpacingBefore( 40F );
-		img.setAlignment( 50 );
+        var imageFile = new ClassPathResource("META-INF/resources/images/logo-for-pdf.png");
+        var imgB = StreamUtils.copyToByteArray(imageFile.getInputStream());
+        final var img = Image.getInstance(imgB);
+        img.setSpacingBefore(40F);
+        img.setAlignment(50);
 
-		img.scaleAbsolute( 64f, 64f );
-		document.add( img );
+        img.scaleAbsolute(64f, 64f);
+        document.add(img);
 
-		final var table = new PdfPTable( 6 );
-		table.setWidthPercentage( 100f );
-		table.setWidths( new float[]{ 2.5f, 2.5f, 4.5f, 2f, 1.5f, 1.5f } );
-		table.setSpacingBefore( 50F );
-		table.setSpacingAfter( 100F );
+        final var table = new PdfPTable(6);
+        table.setWidthPercentage(100f);
+        table.setWidths(new float[]{2.5f, 2.5f, 4.5f, 2f, 1.5f, 1.5f});
+        table.setSpacingBefore(50F);
+        table.setSpacingAfter(100F);
 
-		writeTableHeader( table );
-		writeTableData( table, checks );
+        writeTableHeader(table);
+        writeTableData(table, checks);
 
-		document.add( table );
+        document.add(table);
 
-		final var organizer = checks.get( 0 ).getOrganizer();
-		final var name = organizer.getFirstName() + " " + organizer.getLastName();
-		final var email = organizer.getEmail();
+        final var organizer = checks.get(0).getOrganizer();
+        final var name = organizer.getFirstName() + " " + organizer.getLastName();
+        final var email = organizer.getEmail();
 
-		final var pi = "Met vriendelijke groeten,\n" +
-				"\n" +
-				"\n" +
-				name + "\n" +
-				email + " \n" +
-				"\n" +
-				"\n" +
-				"INTEC BRUSSEL vzw\n" +
-				"Rouppeplein 16 te 1000 Brussel\n" +
-				"Tel: 02 411 29 07\n" +
-				"\n" +
-				"\n" +
-				"Ondernemersnummer: 0475.319.893\n" +
-				"RPR Brussel\n" +
-				"www.intecbrussel.be  - meer kansen op werk!";
+        final var pi = "Met vriendelijke groeten,\n" +
+                "\n" +
+                "\n" +
+                name + "\n" +
+                email + " \n" +
+                "\n" +
+                "\n" +
+                "INTEC BRUSSEL vzw\n" +
+                "Rouppeplein 16 te 1000 Brussel\n" +
+                "Tel: 02 411 29 07\n" +
+                "\n" +
+                "\n" +
+                "Ondernemersnummer: 0475.319.893\n" +
+                "RPR Brussel\n" +
+                "www.intecbrussel.be  - meer kansen op werk!";
 
-		final var fFont = FontFactory.getFont( FontFactory.TIMES_ROMAN );
-		fFont.setSize( 10F );
-		fFont.setColor( Color.BLACK );
+        final var fFont = FontFactory.getFont(FontFactory.TIMES_ROMAN);
+        fFont.setSize(10F);
+        fFont.setColor(Color.BLACK);
 
-		final var p = new Paragraph( pi, fFont );
-		p.setAlignment( Paragraph.ALIGN_LEFT );
+        final var p = new Paragraph(pi, fFont);
+        p.setAlignment(Paragraph.ALIGN_LEFT);
 
-		document.add( p );
+        document.add(p);
 
-		document.close();
+        document.close();
 
-	}
-
-
-	private void writeTableHeader( PdfPTable table ) {
-
-		final var cell = new PdfPCell();
-		cell.setBackgroundColor( Color.BLUE );
-		cell.setPadding( 2 );
-
-		final var font = FontFactory.getFont( FontFactory.HELVETICA );
-		font.setSize( 10F );
-		font.setColor( Color.WHITE );
-
-		cell.setPhrase( new Phrase( "Familienaam", font ) );
-		table.addCell( cell );
-
-		cell.setPhrase( new Phrase( "Voornaam", font ) );
-		table.addCell( cell );
-
-		cell.setPhrase( new Phrase( "E-mail", font ) );
-		table.addCell( cell );
-
-		cell.setPhrase( new Phrase( "Datum", font ) );
-		table.addCell( cell );
-
-		cell.setPhrase( new Phrase( "In", font ) );
-		table.addCell( cell );
-
-		cell.setPhrase( new Phrase( "Uit", font ) );
-		table.addCell( cell );
-	}
+    }
 
 
-	private void writeTableData( PdfPTable table, List< Check > checks ) {
+    private void writeTableHeader(PdfPTable table) {
 
-		final var cFont = FontFactory.getFont( FontFactory.TIMES_ROMAN );
-		cFont.setSize( 10F );
+        final var cell = new PdfPCell();
+        cell.setBackgroundColor(Color.BLUE);
+        cell.setPadding(2);
 
-		for ( Check check : checks ) {
-			table.addCell( new Phrase( check.getAttendee().getLastName(), cFont ) );
-			table.addCell( new Phrase( check.getAttendee().getFirstName(), cFont ) );
-			table.addCell( new Phrase( check.getAttendee().getEmail(), cFont ) );
-			table.addCell( new Phrase( check.getType().toString(), cFont ) );
-			table.addCell( new Phrase( check.getAtTime().toString(), cFont ) );
-		}
-	}
+        final var font = FontFactory.getFont(FontFactory.HELVETICA);
+        font.setSize(10F);
+        font.setColor(Color.WHITE);
+
+        cell.setPhrase(new Phrase("Familienaam", font));
+        table.addCell(cell);
+
+        cell.setPhrase(new Phrase("Voornaam", font));
+        table.addCell(cell);
+
+        cell.setPhrase(new Phrase("E-mail", font));
+        table.addCell(cell);
+
+        cell.setPhrase(new Phrase("Datum", font));
+        table.addCell(cell);
+
+        cell.setPhrase(new Phrase("Tijd", font));
+        table.addCell(cell);
+
+        cell.setPhrase(new Phrase("Typ", font));
+        table.addCell(cell);
+    }
+
+
+    private void writeTableData(PdfPTable table, List<Check> checks) {
+
+        final var cFont = FontFactory.getFont(FontFactory.TIMES_ROMAN);
+        cFont.setSize(10F);
+
+        for (Check check : checks) {
+            table.addCell(new Phrase(check.getAttendee().getLastName(), cFont));
+            table.addCell(new Phrase(check.getAttendee().getFirstName(), cFont));
+            table.addCell(new Phrase(check.getAttendee().getEmail(), cFont));
+            table.addCell(new Phrase(check.getOnDate().toString(), cFont));
+            table.addCell(new Phrase(check.getAtTime().toString(), cFont));
+            table.addCell(new Phrase(check.getType().toString(), cFont));
+        }
+    }
 
 }
