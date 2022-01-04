99e12f879c77999a96482eba39c8c179bb8a9ba7
yilmazchef <yilmaz@mail.be>
2022-01-04 02:38:13 +0100

    as usual hacking time and date issues are simplified.

--- src/main/java/it/vkod/models/entities/Check.java
+++ src/main/java/it/vkod/models/entities/Check.java
@@ -14,6 +14,7 @@ import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import java.io.Serializable;
+import java.time.LocalDate;
 import java.time.LocalTime;
 
 @ToString(onlyExplicitlyIncluded = true)
@@ -136,7 +137,7 @@ public class Check implements Serializable, Cloneable, Persistable<Long> {
 
     @PrePersist
     public void prePersist() {
-
+        this.onDate = java.sql.Date.valueOf(LocalDate.now());
         this.atTime = java.sql.Time.valueOf(LocalTime.now());
     }
 
