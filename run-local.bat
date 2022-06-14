8a71e98ac22ec8b01e9673793cc59e2e725a1ca1
Yilmaz MUSTAFA <yilmaz.mustafa@intecbrussel.be>
2022-06-14 16:50:10 +0200

    dockerfile is recoded.

--- run-local.bat
+++
@@ -1,1 +0,0 @@
-java -jar .\target\checkinout.jar --CHECKINOUT_DATASOURCE_URL="jdbc:mysql://localhost:3306/checkinout_dev?createDatabaseIfNotExist=true&autoReconnect=true&useSSL=false&allowPublicKeyRetrieval=true" --CHECKINOUT_DATASOURCE_USERNAME="root" --CHECKINOUT_DATASOURCE_PASSWORD="toor"
